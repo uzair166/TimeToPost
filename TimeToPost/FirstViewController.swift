0028ef2d9e46f97439916e70a413f1fd80f824b9
Uzair Ishaq <uzair.i@hotmail.co.uk>
2019-04-06 02:21:25 +0100

    Initial Commit

---
+++ TimeToPost/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TimeToPost
+//
+//  Created by Uzair Ishaq on 06/04/2019.
+//  Copyright © 2019 Uzair Ishaq. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TimeToPost/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TimeToPost
+//
+//  Created by Uzair Ishaq on 06/04/2019.
+//  Copyright © 2019 Uzair Ishaq. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
